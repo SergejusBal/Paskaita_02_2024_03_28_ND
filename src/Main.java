f9889804797cc60d3aa3f10abab9106dda48d2d0
Sergejus Balciunas <Sergejus.balciunas@gmail.com>
2024-03-31 11:35:22 +0300

    Pabaigti ND

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,66 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //////////////////////////////////////////////
+        //////// Pirma užduotis
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println();
+        System.out.println("Pirma užduotis");
+        System.out.println();
+
+        int gimimoMetai = 1990;
+        int amzius;
+
+        while (2024 >= gimimoMetai){
+            amzius = 2024 - gimimoMetai;
+            if (amzius > 18) System.out.println("Vyrensis nei 18, amzius yra: " + amzius + " metai.");
+            else if (amzius < 18) System.out.println("Jaunesnis nei 18, amzius yra: " + amzius + " metai.");
+            else System.out.println("18 metu, Pilnametis!");
+
+            gimimoMetai++;
+        }
+
+
+        //////////////////////////////////////////////
+        //////// Antra užduotis
+
+        System.out.println();
+        System.out.println("Antra užduotis");
+        System.out.println();
+
+
+        //Cia skirtingi variantai, kad visus patikrinti ar kiekvienam skirtinga programa?
+        int[] dalikliai = {2, 5, 7, 8, 12, 16, 42, 103, 111, 223, 250};
+
+        // pirmas variantas su visais dalikliais kartu
+        int dalijasi = 0;
+        for(int i = 1; i <= 10000; i++) {
+            for (int j = 0; j < dalikliai.length; j++) {
+                dalijasi = i % dalikliai[j];
+                if (dalijasi == 0) break;
+            }
+            switch (dalijasi) {
+                case 0:
+                    System.out.println(i);
+                    break;
+                default:
+                    System.out.println(i+" nesidalina");
+            }
+        }
+
+        // Antras variantas su kiekvienu dalikliu atskirai
+        for (int j = 0; j < dalikliai.length; j++) {
+            int daliklis = dalikliai[j];
+            for (int i = 1; i <= 10000; i++) {
+                dalijasi = i % daliklis;
+                switch (dalijasi) {
+                    case 0:
+                        System.out.println(i);
+                        break;
+                    default:
+                        System.out.println(i + " nesidalina");
+                }
+            }
         }
     }
 }
